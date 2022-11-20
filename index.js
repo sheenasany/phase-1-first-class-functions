a4fcbf63f4b40372caa959b08c736ab0c2d6e227
Sheena Sang <sheenasany@gmail.com>
2022-11-20 14:17:31 -0500

    commit tree 64362015e74e351097f5fa9c1f70bb65809b0a04

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    callback()
+}
+
+function returnsANamedFunction() {
+ return returnsANamedFunction
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        
+    }
+} 
